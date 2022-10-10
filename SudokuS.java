83ea09e94506f52ee01d901e024cc68beb2daf54
Rebecca <100418094+REBECCA988@users.noreply.github.com>
2022-10-10 08:33:34 +0200

    10-10-2022 update 4
    Updated code, not much has changed and there is a lot of unfinished code. The player class is not done yet. It still has to give the user top three times in sorted order, get all times for a specific user, and check that name is valid and new players don't give usernames already in the file. We also have to create a way to present our game. Text based user interface is acceptable and using java swing to create a layout.

--- SudokuS.java
+++ SudokuS.java
@@ -1,26 +1,113 @@
 import java.util.Random;
 import java.util.Scanner ;
 
+
 public class SudokuS{
 
-
   public static void main(String [] args){
-    Board hello = new Board();
-    int lev = hello.Level();
-
-	hello.MakeGameBoard() ;
-
+	//WELCOME 
 	Scanner in = new Scanner(System.in) ;
-	System.out.println("\nWhat would you like to input:") ;
-	int input = in.nextInt() ;
-	System.out.println("Row:") ;
-	int r = in.nextInt() ;
-	System.out.println("Column:") ;
-	int c = in.nextInt() ;
+	System.out.println("WELCOME TO SUDOKU!") ;
+	System.out.println("Are you a new player <y> for yes  <anything else> for no") ;
+	String s = in.nextLine() ;
+	System.out.println("Please provide a username:") ;
+	String name = in.nextLine() ;
+	Player p = new Player(name) ;
 	
-	Input i = new Input(r,c,input, hello) ;
-	i.Add() ;
-	hello.DisplayBoard() ;
+	if ((s.toLowerCase()).equals("y")) {
+		p.CheckName(name) ; 
+	} 
+	
+	//OPTIONS
+	System.out.println("What do you wish to do:") ; 
+	System.out.println("[1] Player Details") ;
+	System.out.println("[2] New Game") ;
+	System.out.println("[q] Quit") ;
+	String opt = in.nextLine() ;
+	//Player p = new Player(name) ;
+	switch (opt) {
+		case "q" : case "Q": //QUIT
+			return ;
+			
+		case "1" : //PLAYER DETAILS
+			System.out.println("What do you wish to do:") ; 
+			System.out.println("[1] View Times") ;
+			System.out.println("[2] Top Three Times") ;
+			System.out.println("[3] Scoreboard") ;
+			System.out.println("[q] Quit") ;
+			String opt1 = in.nextLine() ;
+			//Player p = new Player(name) ;
+			switch (opt1) {	
+				case "1" :
+					//p.getTimes() ;
+					break ;
+				case "2" :
+					p.TopThreeTimes() ;
+					break ;
+				case "3" :
+					//p.Scoreboard() ;
+					break ;
+				default:
+                    System.out.printf("Command %s not valid.  Please reenter...", opt1);
+                    break;	
+			} //case 1 switch		
+					
+			//System.out.println(p.toString()) ;
 
-  }
+			break ;
+			
+			
+			
+		case "2" : //NEW GAME
+			//BOARD
+			Board hello = new Board(); //call board class
+			int lev = hello.Level();
+			
+			//TIMER
+			long timeMillis = System.currentTimeMillis();
+			long startTime , endTime;
+			double time;
+			startTime = System.currentTimeMillis();
+			endTime = System.currentTimeMillis();
+			time = (endTime-startTime)/1000.0;
+			//System.out.println(time);
+			
+			
+			//MAKE BOARD
+			hello.MakeGameBoard() ; //create board and remove numbers
+			
+			//INPUT
+			while (hello.checkIfEmpty() == false ) {	//while the board still has open spaces
+				//get row, column and input from player
+				System.out.println("\nWhat would you like to input:") ;
+				int input = in.nextInt() ;
+				System.out.println("Row:") ;
+				int r = in.nextInt() ;
+				System.out.println("Column:") ;
+				int c = in.nextInt() ;
+
+				if (hello.ValidInput(r,c,input) == false ) { //if invalid row, column or input
+					System.out.println("Input not entered!") ; 
+				} //if
+				else {
+					hello.DisplayBoard(hello.getBoard()) ;
+				} //else
+				
+			} //while
+
+			//TIMER
+			endTime = System.currentTimeMillis(); 
+			time = (endTime-startTime)/1000.0;
+
+			//OUTPUT AT END OF GAME
+			System.out.println("GAME COMPLETED!") ;
+			System.out.println("You completed this game in: " +  p.AddTime(time) ) ;
+			break ;
+			
+		default:
+			System.out.printf("Command %s not valid.  Please reenter...", opt) ;
+			break;	
+	} //switch		
+
+  } //main
 }
